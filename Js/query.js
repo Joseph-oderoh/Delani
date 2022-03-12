229047651938562e129b426c1746feb8d2226ff2
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-03-12 16:00:12 +0300

    refactor:body-optimized  styling of portfolio section

--- Js/query.js
+++ Js/query.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
 
+   // what we do section
     $("#desn1").click( () => {
         $("#design").toggle();
     })
@@ -10,4 +10,51 @@ $(document).ready(function(){
         $("#product").toggle();
     });
 
-  }); 
+  
+    $("#BlvckEXp").mouseover(function(){
+        $("#overlay").show();
+      }).mouseout(function(){
+        $("#overlay").hide();
+      });
+   
+    $("#Ontario").mouseover(function(){
+        $("#overlay2").show();
+      }).mouseout(function(){
+        $("#overlay2").hide();
+      });
+
+      $("#triangles").mouseover(function(){
+        $("#overlay3").show();
+      }).mouseout(function(){
+        $("#overlay3").hide();
+      });
+
+    $("#jimCarrey").mouseover(function(){
+        $("#overlay4").show();
+      }).mouseout(function(){
+        $("#overlay4").hide();
+      });
+
+      $("#good").mouseover(function(){
+        $("#overlay5").show();
+      }).mouseout(function(){
+        $("#overlay5").hide();
+      });
+
+      $("#cal").mouseover(function(){
+        $("#overlay6").show();
+      }).mouseout(function(){
+        $("#overlay6").hide();
+      });
+
+      $("#Burned-C").mouseover(function(){
+        $("#overlay7").show();
+      }).mouseout(function(){
+        $("#overlay7").hide();
+      });
+
+      $("#silver").mouseover(function(){
+        $("#overlay8").show();
+      }).mouseout(function(){
+        $("#overlay8").hide();
+      });
